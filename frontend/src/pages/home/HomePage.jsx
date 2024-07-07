e57959d27e5e8aed4e54550ba92c1a5e035aaebf
baotran <tranntnbse172980@fpt.edu.vn>
2024-07-07 21:44:39 +0700

    fix posts

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -4,6 +4,7 @@ import Posts from "../../components/common/Posts";
 import CreatePost from "./CreatePost";
 const HomePage = () => {
   const [feedType, setFeedType] = useState("forYou");
+
   return (
     <>
       <div className="flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen">
@@ -35,7 +36,7 @@ const HomePage = () => {
         <CreatePost />
 
         {/* POSTS */}
-        <Posts />
+        <Posts feedType={feedType} />
       </div>
     </>
   );
